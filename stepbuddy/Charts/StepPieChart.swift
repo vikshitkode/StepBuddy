c873a4687f2af6b717122bb58477c0cfff18b2a5
Kode Sai Vikshit <kodevikshit2000@gmail.com>
2025-05-28 22:40:21 -0700

    pie chart interactivity

--- stepbuddy/Charts/StepPieChart.swift
+++ stepbuddy/Charts/StepPieChart.swift
@@ -10,6 +10,19 @@ import Charts
 
 struct StepPieChart: View {
     
+    @State private var rawSelectedChartValue: Double? = 0
+    
+    var selectedWeekDay: WeekDayChartData? {
+        guard let rawSelectedChartValue else { return nil }
+            var total = 0.0
+            
+            return chartData.first{
+                total = total + $0.value
+                return rawSelectedChartValue <= total
+            }
+        
+    }
+    
     var chartData: [WeekDayChartData]
     
     var body: some View {
@@ -25,9 +38,27 @@ struct StepPieChart: View {
                 ForEach(chartData) { weekday in
                     SectorMark(angle: .value("Average Steps", weekday.value),
                                innerRadius: .ratio(0.618),
-                               angularInset: 1).foregroundStyle(.pink.gradient).cornerRadius(6)
+                               outerRadius: selectedWeekDay?.date.weekdayInt == weekday.date.weekdayInt ? 140 : 110,
+                               angularInset: 1).foregroundStyle(.pink.gradient).cornerRadius(6).opacity(selectedWeekDay?.date.weekdayInt == weekday.date.weekdayInt ? 1 : 0.3)
                 }
-            }.frame(height: 240)
+            }
+            .chartAngleSelection(value: $rawSelectedChartValue.animation(.easeInOut))
+            .frame(height: 240)
+            .chartBackground { proxy in
+                GeometryReader { geo in
+                    if let plotFrame = proxy.plotFrame{
+                        let frame = geo[plotFrame]
+                        if let selectedWeekDay{
+                            VStack{
+                                Text(selectedWeekDay.date.weekdayTitle).font(.title3.bold()).contentTransition(.identity)
+                                
+                                Text(selectedWeekDay.value, format: .number.precision(.fractionLength(0))).fontWeight(.medium).foregroundStyle(.secondary).contentTransition(.numericText())
+                            }
+                            .position(x: frame.midX, y: frame.midY)
+                        }
+                    }
+                }
+            }
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
