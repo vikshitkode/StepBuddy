3f3d9cd1652f85ec04282a35bc2c996ad6711c20
Kode Sai Vikshit <kodevikshit2000@gmail.com>
2025-08-19 17:35:46 -0700

    Added HeartBeat Image
    Added HeartBeat Image to the project and HKPermissionPrimingView for better User Experience

--- stepbuddy/Screens/HealthKitPermissionPrimingView.swift
+++ stepbuddy/Screens/HealthKitPermissionPrimingView.swift
@@ -23,18 +23,34 @@ struct HealthKitPermissionPrimingView: View {
     
     var body: some View {
         VStack(spacing: 130){
-            VStack(alignment: .leading, spacing: 10){
+            ZStack{
+                Image(.heartBeat)
+                    .resizable()
+                    .frame(width: 300, height: 180)
+                    .opacity(0.1).padding()
+                    .offset(x: 60, y: -95)
                 
-                Image(.appleHealth).resizable().frame(width: 90, height: 90).shadow(color: .gray.opacity(0.5), radius: 16).padding(.bottom, 12)
-                
-                Text("Apple Health Integration").font(.title2).bold()
-                
-                Text(description).foregroundStyle(.secondary)
+                VStack(alignment: .leading, spacing: 10){
+                    
+                    Image(.appleHealth).resizable()
+                        .frame(width: 90, height: 90)
+                        .shadow(color: .gray.opacity(0.5), radius: 16)
+                        .padding(.bottom, 12)
+                    
+                    Text("Apple Health Integration")
+                        .font(.title2)
+                        .bold()
+                    
+                    Text(description)
+                        .foregroundStyle(.secondary)
+                }
             }
             
             Button("Connect Apple Health"){
                 isShowingHealthKitPermissions = true
-            }.buttonStyle(.borderedProminent).tint(.pink)
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
 
         }.padding(30)
             .interactiveDismissDisabled()
