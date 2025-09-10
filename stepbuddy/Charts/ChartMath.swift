efe7c5568e3c7c15028b263d315212c720fdf30b
Kode Sai Vikshit <kodevikshit2000@gmail.com>
2025-09-10 00:30:11 -0700

    weight diff calculation

--- stepbuddy/Charts/ChartMath.swift
+++ stepbuddy/Charts/ChartMath.swift
@@ -28,4 +28,35 @@ struct ChartMath {
         return weekdayChartData
     }
     
+    static func avgDailyWeightDiff(for weights: [HealthMetric]) -> [WeekDayChartData] {
+        
+        var diffValues: [(date: Date, value: Double)] = []
+        
+        for i in 0..<weights.count {
+            let date = weights[i].date
+            let diff = weights[i].value - weights[i-1].value
+            
+            diffValues.append((date: date, value: diff))
+        }
+        
+//        for value in diffValues {
+//            print("\(value.date), \(value.value)")
+//        }
+        
+        let sortedByWeekDay = diffValues.sorted { $0.date.weekdayInt < $1.date.weekdayInt }
+        let weekdayArray = sortedByWeekDay.chunked { $0.date.weekdayInt == $1.date.weekdayInt }
+        
+        var weekDayChartData: [WeekDayChartData] = []
+        
+        for array in weekdayArray {
+            guard let firstValue = array.first else { continue }
+            let total = array.reduce(0) { $0 + $1.value }
+            let avgWeightDiff = total/Double(array.count)
+            
+            weekDayChartData.append(.init(date: firstValue.date, value: avgWeightDiff))
+        }
+        
+        return weekDayChartData
+    }
+    
 }
