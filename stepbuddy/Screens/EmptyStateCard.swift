f3d0f033ebeac0f01b89b8c5409a840219f33a3c
Kode Sai Vikshit <kodevikshit2000@gmail.com>
2025-09-21 00:45:41 -0700

    Fixed value adding crash in empty state app

---
+++ stepbuddy/Screens/EmptyStateCard.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyStateCard.swift
+//  stepbuddy
+//
+//  Created by Sai Vikshit Kode on 9/20/25.
+//
+
+import SwiftUI
+
+struct EmptyStateCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyStateCard()
+}
